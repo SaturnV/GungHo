bc10e7004c8bf9b5666b4b39a66cebc69db7cf54
Adam Helybely <adam@helybely.com>
2012-07-25 18:31:48 +0200

    SmartMatch type

---
+++ lib/GungHo/Type/SmartMatch.pm
@@ -0,0 +1,54 @@
+#! /usr/bin/perl
+# TODO: License
+###### NAMESPACE ##############################################################
+
+package GungHo::Type::SmartMatch;
+
+###### IMPORTS ################################################################
+
+use strict;
+use warnings;
+use feature ':5.10';
+
+use UUID;
+
+###### INIT ###################################################################
+
+use parent qw( GungHo::Type::Any );
+
+###### VARS ###################################################################
+
+our $ModName = __PACKAGE__;
+our $TypeName = $ModName->Name();
+
+###### METHODS ################################################################
+
+sub new
+{
+  my $class = shift;
+  my $parent_type = shift;
+
+  my $self = bless({}, $class);
+  if (defined($parent_type))
+  {
+    $self->{'parent'} = $parent_type;
+    Scalar::Util::weaken($self->{'parent'})
+      if ref($parent_type);
+  }
+
+  $self->{'match'} = \@_;
+
+  return $self;
+}
+
+# $type->Validate($arg)
+sub Validate
+{
+  my $self = shift;
+  die "TODO::TypeError[$TypeName]: Doesn't look good."
+    unless ($_[0] ~~ $self->{'match'});
+}
+
+###### THE END ################################################################
+
+1
