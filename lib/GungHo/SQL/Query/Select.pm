3ec7889123756daefa16807e2aea7f60be24de75
Adam Helybely <adam@helybely.com>
2012-08-22 14:56:20 +0200

    JOINs.

--- lib/GungHo/SQL/Query/Select.pm
+++ lib/GungHo/SQL/Query/Select.pm
@@ -12,8 +12,6 @@ use feature ':5.10';
 
 ###### VARS ###################################################################
 
-my $next_alias = 'a';
-
 ###### METHODS ################################################################
 
 sub new
@@ -27,7 +25,10 @@ sub new
         'select' => [],
         'from' => [],
         'where' => [],
-        'params' => []
+        'where_params' => [],
+        'group_by' => [],
+        'having' => [],
+        'having_params' => []
       };
 
   return bless($self, $class);
@@ -42,15 +43,58 @@ sub AddSelect
 
 sub AddFrom
 {
+  state $next_alias = 'a';
   my $self = shift;
+  my @ret;
+
   die "TODO: Add what?" unless @_;
 
-  my $alias = '__sta_' . $next_alias++;
-  die "TODO: JOINs not implemented"
-    if (@{$self->{'from'}} || $#_);
-  push(@{$self->{'from'}}, "$_[0] $alias");
+  my $alias;
+  foreach (@_)
+  {
+    push(@ret, $alias = '__sta_' . $next_alias++);
+    push(@{$self->{'from'}}, { 'table' => $_[0], 'alias' => $alias });
+  }
 
-  return $alias;
+  return @ret if wantarray;
+  return $ret[0];
+}
+
+sub Join
+{
+  my $self = shift;
+  my $params = GungHo::Utils::make_hashref(@_);
+
+  my $t;
+  my $alias = $params->{'alias'} or
+    die "TODO: Join what?";
+  foreach (@{$self->{'from'}})
+  {
+    if ($_->{'alias'} eq $alias)
+    {
+      $t = $_;
+      last;
+    }
+  }
+  die "TODO: Can't find '$alias'" unless $t;
+  
+  $t->{'join'} = uc($params->{'type'}) || 'JOIN';
+
+  if ($params->{'on'})
+  {
+    $t->{'cond'} = 'ON ' . $params->{'on'};
+
+    my $p = $params->{'params'};
+    $t->{'params'} = ref($p) ? $p : [ $p ] if $p;
+  }
+  elsif (my $u = $params->{'using'})
+  {
+    my @fs = ref($u) ? @{$u} : ($u);
+    local $" = ', ';
+    $t->{'cond'} = "USING (@fs)";
+  }
+
+  return $self;
 }
 
 sub AddWhere
@@ -58,28 +102,54 @@ sub AddWhere
   my $self = shift;
   die "TODO: Add what?" unless @_;
   push(@{$self->{'where'}}, shift);
-  push(@{$self->{'params'}}, @_);
+  push(@{$self->{'where_params'}}, @_);
+  return $self;
+}
+
+sub AddGroupBy
+{
+  my $self = shift;
+  push(@{$self->{'group_by'}}, @_);
+  return $self;
+}
+
+sub AddHaving
+{
+  my $self = shift;
+  die "TODO: Add what?" unless @_;
+  push(@{$self->{'having'}}, shift);
+  push(@{$self->{'having_params'}}, @_);
   return $self;
 }
 
 sub Build
 {
   my $self = shift;
-  my $sql;
+  my ($sql, @params);
 
   {
     my @fs = @{$self->{'select'}};
     die "TODO: No fields SELECTed" unless @fs;
 
     local $" = ', ';
-    $sql = "SELECT @fs"
+    $sql = "SELECT @fs";
   }
 
   {
     my @ts = @{$self->{'from'}};
     die "TODO: No tables SELECTed" unless @ts;
-    die "TODO: JOINs not implemented" if $#ts;
-    $sql .= ' FROM ' . join(', ', @ts);
+    $sql .= ' FROM';
+
+    my $c = 0;
+    foreach (@ts)
+    {
+      $sql .= ($_->{'join'} ? ' ' . $_->{'join'} : ',') if $c++;
+      $sql .= ' ' . $_->{'table'} . ' ' . $_->{'alias'};
+      $sql .= ' ' . $_->{'cond'} if $_->{'cond'};
+
+      push(@params, @{$_->{'params'}})
+        if $_->{'params'};
+    }
   }
 
   if (my @ws = @{$self->{'where'}})
@@ -87,9 +157,24 @@ sub Build
     @ws = map { "($_)" } @ws if $#ws;
     local $" = ' AND ';
     $sql .= " WHERE @ws";
+    push(@params, @{$self->{'where_params'}});
   }
 
-  return ($sql, @{$self->{'params'}});
+  if (my @gs = @{$self->{'group_by'}})
+  {
+    local $" = ', ';
+    $sql .= " GROUP BY @gs";
+  }
+
+  if (my @hs = @{$self->{'having'}})
+  {
+    @hs = map { "($_)" } @hs if $#hs;
+    local $" = ' AND ';
+    $sql .= " HAVING @hs";
+    push(@params, @{$self->{'having_params'}});
+  }
+
+  return ($sql, @params);
 }
 
 ###### THE END ################################################################
